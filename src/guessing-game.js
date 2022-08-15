4e44bc3d3804d8a647c642840a56a870ce82cffb
Elizaveta Kuskova <79181717290@ya.ru>
2022-08-15 11:03:59 +0300

    doc: add src

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
-class GuessingGame {
+class { 
     constructor() {}
 
-    setRange(min, max) {
-
+    setRange(min, max)  {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid
     }
 }
 
